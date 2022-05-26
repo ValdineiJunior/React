43d8fced00901742887bbcd3aa5661ddf9ff38de
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2022-05-26 01:42:23 -0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>linha</li>
+          <li>linha</li>
+          <li>linha</li>
+        </ul>
+      </Header>
+
+      <Header title="Projects">
+        <ul>
+          <li>linha</li>
+          <li>linha</li>
+          <li>linha</li>
+        </ul>
+      </Header>
+    </>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import { soma } from './soma'
+import React from 'react'
+import { render } from 'react-dom'
 
-console.log(soma(1, 3))
+import App from './App'
+
+render(<App />, document.getElementById('app'))
